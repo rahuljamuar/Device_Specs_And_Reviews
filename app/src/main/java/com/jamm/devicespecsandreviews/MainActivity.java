5646fa41d1743267c9214f5bd56814ebc7a74aa3
Rahul <rahul.jamuar007@gmail.com>
2016-04-08 18:35:22 +0530

    Second commit

--- app/src/main/java/com/jamm/devicespecsandreviews/MainActivity.java
+++ app/src/main/java/com/jamm/devicespecsandreviews/MainActivity.java
@@ -8,8 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button apple, samsung, motorola, lg, sony, lenovo, oneplus, mi, nexus, htc, asus, huawei, oppo, lumia, micromax, yu, gionee, panasonic, karbonn, lava;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +21,170 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        apple=(Button)findViewById(R.id.apple);
+        samsung=(Button)findViewById(R.id.samsung);
+        motorola=(Button)findViewById(R.id.motorola);
+        lg=(Button)findViewById(R.id.lg);
+        sony=(Button)findViewById(R.id.sony);
+        lenovo=(Button)findViewById(R.id.lenovo);
+        oneplus=(Button)findViewById(R.id.oneplus);
+        mi=(Button)findViewById(R.id.mi);
+        nexus=(Button)findViewById(R.id.nexus);
+        htc=(Button)findViewById(R.id.htc);
+        asus=(Button)findViewById(R.id.asus);
+        huawei=(Button)findViewById(R.id.huawei);
+        oppo=(Button)findViewById(R.id.oppo);
+        lumia=(Button)findViewById(R.id.lumia);
+        micromax=(Button)findViewById(R.id.micromax);
+        yu=(Button)findViewById(R.id.yu);
+        gionee=(Button)findViewById(R.id.gionee);
+        panasonic=(Button)findViewById(R.id.panasonic);
+        karbonn=(Button)findViewById(R.id.karbonn);
+        lava=(Button)findViewById(R.id.lava);
+
+        apple.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
             }
         });
+
+        samsung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        motorola.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        lg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        sony.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        lenovo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        oneplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        nexus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        htc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        asus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        huawei.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        oppo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        lumia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        micromax.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        yu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        gionee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        panasonic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        karbonn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        lava.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
     }
 
     @Override
