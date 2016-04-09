5fae5d1292911b46fed534165bc8abd584b7e4e9
Rahul <rahul.jamuar007@gmail.com>
2016-04-09 15:26:35 +0530

    Worked on Oneplus one

--- app/src/main/java/com/jamm/devicespecsandreviews/Oneplus.java
+++ app/src/main/java/com/jamm/devicespecsandreviews/Oneplus.java
@@ -2,6 +2,7 @@ package com.jamm.devicespecsandreviews;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,11 +10,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,10 +40,14 @@ public class Oneplus extends AppCompatActivity {
     private Spinner modelSpinner;
     ArrayList<String> listModels = new ArrayList<String>();
     ArrayList<String> imageLinkList = new ArrayList<String>();
+    ArrayList<String> listTechnology = new ArrayList<String>();
     NetworkImageView networkModelImage;
+    private PopupWindow popupWindow;
+    private LayoutInflater layoutInflater;
+    private FrameLayout relativeLayout;
+    private static int staticPosition;
 
-
-
+    private TextView line1, line2, network, technology, technologyValue;
 
 
     @Override
@@ -56,19 +69,35 @@ public class Oneplus extends AppCompatActivity {
 
         modelSpinner = (Spinner) findViewById(R.id.modelSpinner);
         networkModelImage = (NetworkImageView) findViewById(R.id.modelPhoto);
+        relativeLayout = (FrameLayout) findViewById(R.id.oneplusLayout);
+        line1 = (TextView) findViewById(R.id.line1);
+        line2 = (TextView) findViewById(R.id.line2);
+        network = (TextView) findViewById(R.id.network);
+        technology = (TextView) findViewById(R.id.technology);
+        technologyValue = (TextView) findViewById(R.id.technologyValue);
 
         modelSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                staticPosition = position;
+                if (position != 0) {
+                    line1.setVisibility(View.VISIBLE);
+                    line2.setVisibility(View.VISIBLE);
+                    network.setVisibility(View.VISIBLE);
+                    technology.setVisibility(View.VISIBLE);
+                    technologyValue.setVisibility(View.VISIBLE);
+                    networkModelImage.setVisibility(View.VISIBLE);
+                    getImage(imageLinkList.get(position).toString());
+                    technologyValue.setText(listTechnology.get(position).toString());
 
-                    if(position == 0){
-                        networkModelImage.setImageResource(R.drawable.oneplus);
-                    }
-                    else{
-                        getImage(imageLinkList.get(position).toString());
-                    }
-
-
+                } else {
+                    line1.setVisibility(View.GONE);
+                    line2.setVisibility(View.GONE);
+                    network.setVisibility(View.GONE);
+                    technology.setVisibility(View.GONE);
+                    technologyValue.setVisibility(View.GONE);
+                    networkModelImage.setVisibility(View.GONE);
+                }
 
 
             }
@@ -80,6 +109,52 @@ public class Oneplus extends AppCompatActivity {
             }
         });
 
+        networkModelImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.view_image, null);
+
+                popupWindow = new PopupWindow(container, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, true);
+                //popupWindow.setBackgroundDrawable(new BitmapDrawable(null,""));
+
+                popupWindow.showAtLocation(relativeLayout, Gravity.CENTER, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                relativeLayout.getForeground().setAlpha(220);
+
+                NetworkImageView bigNetworkImage = (NetworkImageView) container.findViewById(R.id.biggerModelPhoto);
+                Button closePopup = (Button) container.findViewById(R.id.closePopup);
+
+                closePopup.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        relativeLayout.getForeground().setAlpha(0);
+                        popupWindow.dismiss();
+                    }
+                });
+
+                ImageLoader bigImageLoader = AppController.getInstance().getImageLoader();
+
+                bigNetworkImage.setImageUrl(imageLinkList.get(staticPosition).toString(), bigImageLoader);
+
+
+
+
+
+               /* container.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        relativeLayout.getForeground().setAlpha(0);
+                        popupWindow.dismiss();
+                        return false;
+                    }
+                });*/
+
+
+            }
+        });
+
+        relativeLayout.getForeground().setAlpha(0);
 
 
     }
@@ -96,13 +171,13 @@ public class Oneplus extends AppCompatActivity {
     public void addItemsOnSpinner() {
 
 
-
         ReadExcel readExcel = new ReadExcel();
-        readExcel.readModels();
+        readExcel.readModels("Oneplus");
 
 
         listModels = readExcel.getListModels();
-        imageLinkList=readExcel.getImageLinkList();
+        imageLinkList = readExcel.getImageLinkList();
+        listTechnology = readExcel.getListTechnology();
         modelSpinner = (Spinner) modelSpinner.findViewById(R.id.modelSpinner);
 
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
@@ -113,15 +188,13 @@ public class Oneplus extends AppCompatActivity {
 
     }
 
-    private void getImage(String url){
-        ImageLoader imageLoader=AppController.getInstance().getImageLoader();
+    private void getImage(String url) {
+        ImageLoader imageLoader = AppController.getInstance().getImageLoader();
 
-        networkModelImage.setImageUrl(url,imageLoader);
+        networkModelImage.setImageUrl(url, imageLoader);
 
 
     }
-
-
 
 
 }
--- app/src/main/java/com/jamm/devicespecsandreviews/ReadExcel.java
+++ app/src/main/java/com/jamm/devicespecsandreviews/ReadExcel.java
@@ -22,16 +22,21 @@ public class ReadExcel {
 
     ArrayList<String> listModels = new ArrayList<String>();
     ArrayList<String> imageLinkList = new ArrayList<String>();
+    ArrayList<String> listTechnology = new ArrayList<String>();
 
     public ArrayList<String> getListModels() {
         return listModels;
+    }
+
+    public ArrayList<String> getListTechnology() {
+        return listTechnology;
     }
 
     public ArrayList<String> getImageLinkList() {
         return imageLinkList;
     }
 
-    public void readModels() {
+    public void readModels(String smartPhoneBrand) {
 
         try {
             File SDCardRoot = Environment.getExternalStorageDirectory();
@@ -40,13 +45,13 @@ public class ReadExcel {
 
             POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);
             HSSFWorkbook myWorkBook = new HSSFWorkbook(myFileSystem);
-            HSSFSheet mySheet = myWorkBook.getSheet("Oneplus");
+            HSSFSheet mySheet = myWorkBook.getSheet(smartPhoneBrand);
 
             rowCount = mySheet.getPhysicalNumberOfRows();
 
 
-
-            for (int k = 0; k < rowCount; k++) {
+            listModels.add("Select Model");
+            for (int k = 1; k < rowCount; k++) {
                 Row row = mySheet.getRow(k);
                 Cell cell = null;
                 cell = row.getCell(0);
@@ -71,6 +76,20 @@ public class ReadExcel {
                 } else {
                     cellVal = "";
                     imageLinkList.add(cellVal);
+                }
+            }
+
+            for (int k = 0; k < rowCount; k++) {
+                Row row = mySheet.getRow(k);
+                Cell cell = null;
+                cell = row.getCell(2);
+                if (cell != null) {
+                    cell.setCellType(Cell.CELL_TYPE_STRING);
+                    cellVal = cell.toString();
+                    listTechnology.add(cellVal);
+                } else {
+                    cellVal = "";
+                    listTechnology.add(cellVal);
                 }
             }
 
